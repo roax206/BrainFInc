3ef690416c69c1f91da3831dd9f65ba481a2a86c
roax206 <r.o.a.fan.roax@gmail.com>
2022-10-30 23:35:58 +1100

    Update Run.java

--- Run.java
+++ Run.java
@@ -1,18 +1,17 @@
 import java.io.File;
 import java.io.PrintStream;
+import java.io.IOException;
 
 import java.util.Scanner;
 
 public class Run
 {
-	public Scanner input = null;
-	public PrintStream output = null;
-	public char[] memory = null;
+	public static Scanner input = new Scanner(System.in);
+	public static PrintStream output = System.out;
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-		char[] memory = new char[256]();
-		Scanner src = null;
+		String src = null;
 		
 		if(args.length > 0 && args.length <=3)
 		{
@@ -24,7 +23,7 @@ public class Run
 					case 2:
 					input = new Scanner(new File(args[1]));
 					case 1:
-					src = new Scanner(new File(args[0]));
+					src = args[0];
 				}
 			}
 			catch(IOException e)
@@ -36,6 +35,11 @@ public class Run
 			{
 				System.out.println("source file: \"" + args[0] + "\" could not be read");
 				return;
+			}
+			else
+			{
+				BrainFuncInterpreter proc = new BrainFuncInterpreter(256, input, output, true);
+				proc.process(src);
 			}
 		}
 		else
